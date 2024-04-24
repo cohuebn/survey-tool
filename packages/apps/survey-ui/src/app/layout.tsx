2f4604a8c4adeb56557ea25c9e980c4e1a81d005
Cory Huebner <chuebner86@gmail.com>
2024-04-23 21:47:36 -0500

    added viewport meta tag

--- packages/apps/survey-ui/src/app/layout.tsx
+++ packages/apps/survey-ui/src/app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <FavIcons />
         <title>DocVoice</title>
       </head>
