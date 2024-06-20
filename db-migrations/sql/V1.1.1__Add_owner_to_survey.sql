ac54516a394d55d866c8c5f4abe02de3266fe10b
Cory Huebner <chuebner86@gmail.com>
2024-06-20 15:22:41 -0500

    add owner to surveys

---
+++ db-migrations/sql/V1.1.1__Add_owner_to_survey.sql
@@ -0,0 +1,4 @@
+alter table ${flyway:defaultSchema}.surveys
+  add column owner_id uuid references ${flyway:defaultSchema}.users(user_id);
+
+create index if not exists surveys_owner_id_idx on ${flyway:defaultSchema}.surveys (owner_id);
