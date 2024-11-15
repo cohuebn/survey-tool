093d4ff2ee2901cc53b6b03702a919d859a7529e
Cory Huebner <chuebner86@gmail.com>
2024-11-14 21:44:16 -0600

    add overall rating support to rating questions

--- packages/apps/survey-ui/src/app/surveys/survey-editor/question-definitions/rating-editor.tsx
+++ packages/apps/survey-ui/src/app/surveys/survey-editor/question-definitions/rating-editor.tsx
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material";
+import { FormControlLabel, Switch, TextField } from "@mui/material";
 
 import { QuestionDefinitionProps } from "../question-definition-props";
 import styles from "../styles.module.css";
@@ -8,40 +8,60 @@ export function RatingEditor({
   definition,
   dispatch,
 }: QuestionDefinitionProps) {
+  const includeInOverallRating: boolean = Boolean(
+    definition.includeInOverallRating,
+  );
   return (
-    <div className={styles.minMaxRatingContainer}>
-      <TextField
-        type="number"
-        placeholder="The minimum rating allowed"
-        label="Minimum rating"
-        value={definition?.minRating ?? ""}
-        onChange={(event) =>
-          dispatch({
-            type: "updateQuestionDefinition",
-            questionId,
-            value: {
-              ...definition,
-              minRating: parseInt(event.target.value, 10),
-            },
-          })
+    <>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={includeInOverallRating}
+            onChange={(_, value) => {
+              dispatch({
+                type: "updateQuestionDefinition",
+                questionId,
+                value: { ...definition, includeInOverallRating: value },
+              });
+            }}
+          />
         }
+        label="Include in overall rating"
       />
-      <TextField
-        type="number"
-        placeholder="The maximum rating allowed"
-        label="Maximum rating"
-        value={definition?.maxRating ?? ""}
-        onChange={(event) =>
-          dispatch({
-            type: "updateQuestionDefinition",
-            questionId,
-            value: {
-              ...definition,
-              maxRating: parseInt(event.target.value, 10),
-            },
-          })
-        }
-      />
-    </div>
+      <div className={styles.minMaxRatingContainer}>
+        <TextField
+          type="number"
+          placeholder="The minimum rating allowed"
+          label="Minimum rating"
+          value={definition?.minRating ?? ""}
+          onChange={(event) =>
+            dispatch({
+              type: "updateQuestionDefinition",
+              questionId,
+              value: {
+                ...definition,
+                minRating: parseInt(event.target.value, 10),
+              },
+            })
+          }
+        />
+        <TextField
+          type="number"
+          placeholder="The maximum rating allowed"
+          label="Maximum rating"
+          value={definition?.maxRating ?? ""}
+          onChange={(event) =>
+            dispatch({
+              type: "updateQuestionDefinition",
+              questionId,
+              value: {
+                ...definition,
+                maxRating: parseInt(event.target.value, 10),
+              },
+            })
+          }
+        />
+      </div>
+    </>
   );
 }
